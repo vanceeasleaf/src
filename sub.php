534b4e7607ddb364d83f222c3f9566b88d7f58b3
Yang Zhou <404422239@qq.com>
2014-12-11 20:19:39 +0800

    BN

--- sub.php
+++ sub.php
@@ -4,13 +4,13 @@ $projHome=dirname(__FILE__);
 $projName=basename($projHome);
 if($stage==1){
 	$units="metal";
-	$species="CN-small";
+	$species="BN-small";
 	$method="nvt";
 	$nodes=1;
-$procs=4;$queue="q1.4";
+$procs=4;$queue="q1.2";
 $runTime=10000000;
 for($i=0;$i<31;$i++)	
-submit("\$cell=\"C3N1/$i\";\$thick=1.44;\$langevin=0;\$hdeta=8*\$deta;\$usinglat=1;\$timestep=.5e-3;\n\$latx=11;\$laty=1;\$latz=1;");
+submit("\$cell=\"CN/$i\";\$thick=1.44;\$langevin=0;\$hdeta=8*\$deta;\$usinglat=1;\$timestep=.5e-3;\n\$latx=11;\$laty=1;\$latz=1;");
 }
 shell_exec("cp $projHome/sub.php $srcHome;");
 require_once("$srcHome/submit.php");
