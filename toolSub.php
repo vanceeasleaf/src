35914c310c595c0bb0f37f3c2a2de0379ad53f9e
Yang Zhou <404422239@qq.com>
2014-12-12 18:53:16 +0800

    important update
    in order to follow the limitation of the cluster that force us to use
    20*1 cores,we must wrap multi jobs into a big one .

--- toolSub.php
+++ toolSub.php
@@ -1,5 +1,6 @@
 <?php
 	require_once("genPbs.php");
+	require_once("genPbss.php");
 	require_once("units.php");
 
 if(!$projHome)exit("please set your project path!");
@@ -32,6 +33,7 @@ function submit(){
 	global $species;
 	global $method;
 	global $units;
+	global $universe;
 		global $queue;
 	global $runTime;
 		if(!$queue)$queue="q1.1";
@@ -39,20 +41,62 @@ function submit(){
 	if(!$procs)$procs=12;
 	$para="";
 		makeLoopFile($cmd,$idx);
-	$pid=setSubProject($idx);
+	if(!$universe)$pid=setSubProject($idx);
 	$json_obj=array("id"=>$idx,"pid"=>$pid,"time"=>date('Y-m-d H:i:s'),"cmd"=>$cmd,"nodes"=>$nodes,"procs"=>$procs,"species"=>$species,"method"=>$method,"units"=>$units,"runTime"=>$runTime);
 	//fprintf($loops,"{\"id\":\"%s\",\"pid\":\"%s\",\"time\":\"%s\",\"cmd\":\"%s\",\"status\":\"Q\"",$idx,$pid,date('Y-m-d H:i:s'),$cmd);
 	if($argc>1){
 		$pa=$argv[1];
 		while($key=key($pa)){
 				$json_obj[$key]=$pa[$key];
-				$para=sprintf("$para,\"%s\":%s",$key,$pa[$key]);
+				//$para=sprintf("$para,\"%s\":%s",$key,$pa[$key]);
 				next($pa);
 		}
 	}
 	fprintf($loops,"%s\n",json_encode($json_obj));
-		write($cmd.";\n\$projHome=\"$projHome/$idx\";".$para,"$projHome/$idx/cmd.txt");
+		//write($cmd.";\n\$projHome=\"$projHome/$idx\";".$para,"$projHome/$idx/cmd.txt");
 	$idx++;
+}
+function uexec(){
+	global $projHome;
+	$home=dirname(__FILE__);
+	shell_exec("cd $projHome/pbs;ls *.pbs>tmp;");
+	$fp=fopen("$projHome/pbs/tmp","r");
+	$n=0;
+	while(list($st[$n],$ed[$n])=fscanf($fp,"lammps%d-%d.pbs")){
+		$n++;
+	}
+	//sort
+	$len=$n;
+	for($i=0;$i<$len;$i++){
+		$idx[$i]=$i;
+	}
+	for($i=0;$i<$len;$i++)
+		for($j=0;$j<$i;$j++){
+			if($st[$i]<$st[$j]){
+				swap($st[$i],$st[$j]);
+				swap($ed[$i],$ed[$j]);
+				swap($idx[$i],$idx[$j]);
+			}
+		}
+	$fi=fopen("$projHome/pbs/info","w");
+	for($i=0;$i<$len;$i++){
+		$p=$i;
+		$st1=$st[$p];
+		$ed1=$ed[$p];
+		echo "lammps${st1}-${ed1}.pbs\n";
+		$lb="${st1}-${ed1}";
+		$pid=shell_exec("cd $projHome/pbs;qsub lammps${st1}-${ed1}.pbs;");
+		echo $pid;
+		$pid=trim($pid);
+		for($j=$st1;$j<=$ed1;$j++){
+			$b=$j-$st1;
+			fprintf($fi,"$j\t$pid\tlog.$lb.$b\tscreen.$lb.$b\n");
+		}
+	}	
+	
+}	
+function swap(&$a,&$b){
+	$tmp=$b;$b=$a;$a=$tmp;
 }
 function makeLoopFile($cmd,$idx){
 	global $projHome;
@@ -63,6 +107,21 @@ function makeLoopFile($cmd,$idx){
 	global $queue;
 		global $nodes;
 	global $procs;
+	global $uqueue;
+		global $universe;
+	if($universe){
+		echo "prepared: $projHome/$idx\n";
+		global $unodes;
+		global $uprocs;
+		$cores=$procs*$nodes;
+		if(!$unodes)$unodes=20;
+		if(!$uprocs)$uprocs=1;
+		$ucores=$unodes*$uprocs;
+		$len=floor($ucores/$cores);
+		if(!$uqueue)$uqueue="q3.4";
+		if($idx%$len==0)
+		genPbss("$projHome","zy_$projName"."_",$uqueue,$unodes,$uprocs,$idx,$cores);
+	}
 	shell_exec("mkdir -p $projHome/$idx;cd $projHome/$idx;mkdir -p minimize");
 genPbs("$projHome/$idx","zy_$projName"."_$idx",$queue,$nodes,$procs);
 	write("<?php\n$cmd;\n\$projHome=\"$projHome/$idx\";\n?>","$projHome/$idx/qloop.php");
