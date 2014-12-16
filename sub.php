a0aa5c82842cfa086a6e49b362cebe2e0e3c65e3
Yang Zhou <404422239@qq.com>
2014-12-16 22:11:30 +0800

    graphene isotope and SiGe

--- sub.php
+++ sub.php
@@ -4,17 +4,20 @@ $projHome=dirname(__FILE__);
 $projName=basename($projHome);
 
 	$units="metal";
-	$species="CN-small";
+	$species="CC-small";
 	$method="nvt";
 	$nodes=1;
-$procs=4;$queue="q1.4";
-$uprocs=1;$uqueue="q3.4";
-$unodes=20;
+$procs=4;$queue="q1.1";
+$uprocs=1;
+$unodes=12;$uqueue="q1.2";
 $universe=1;
 $runTime=10000000;
 if($stage==1){
-for($i=0;$i<31;$i++)	
-submit("\$cell=\"C3N/$i\";\$thick=1.44;\$langevin=0;\$hdeta=8*\$deta;\$usinglat=1;\$timestep=.5e-3;\n\$latx=11;\$laty=1;\$latz=1;");
+for($mass=9;$mass<=15;$mass++){
+for($i=0;$i<5;$i++){	
+submit("\$seed=13515;\$mass=$mass;\$cell=\"C12N/$i\";\$thick=1.44;\$langevin=0;\$hdeta=8*\$deta;\$usinglat=1;\$timestep=.5e-3;\n\$latx=11;\$laty=1;\$latz=1;");
+}
+}
 uexec();
 }
 shell_exec("cp $projHome/sub.php $srcHome;");
